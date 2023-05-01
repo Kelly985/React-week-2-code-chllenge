f5c36031fdbd14d4985cdc43a203d0e46e1a1dfe
Kelly985 <86683835+Kelly985@users.noreply.github.com>
2023-05-01 15:33:48 +0300

    set up bot army section

--- bots/src/components/MyBotarmy.js
+++ bots/src/components/MyBotarmy.js
@@ -2,6 +2,28 @@ import React from "react";
 
 function YourBotArmy({ army, dischargeBot, deleteBot }){
 
+  return (
+    <div className="your-bot-army">
+      {army.map((bot) => (
+        <div key={bot.id} className="bot-card">
+          <div className="bot-info">
+            <img src={bot.avatar_url} alt={`${bot.name}'s avatar`} />
+            <div className="bot-details">
+              <h3>{bot.name}</h3>
+              <p>Health: {bot.health}</p>
+              <p>Damage: {bot.damage}</p>
+              <p>Armor: {bot.armor}</p>
+              <p>Class: {bot.bot_class}</p>
+            </div>
+          </div>
+          <div className="bot-buttons">
+            <button onClick={() => dischargeBot(bot)}>Discharge</button>
+            <button onClick={() => deleteBot(bot)}>x</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 
 }
 
