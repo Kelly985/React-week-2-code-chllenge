55f96ea269b4106b7f5fa66313b7039de1497556
Kelly985 <86683835+Kelly985@users.noreply.github.com>
2023-05-01 15:31:55 +0300

    displayed my bot collection

--- bots/src/components/BotCollection.js
+++ bots/src/components/BotCollection.js
@@ -2,7 +2,22 @@ import React from "react";
 
 function BotCollection({ bots, enlistBot }) {
 
-  
+  return (
+    <div>
+      {bots.map((bot) => (
+        <div key={bot.id} className="bot">
+          <h3>{bot.name}</h3>
+          <img src={bot.avatar_url} alt={`${bot.name}'s avatar`} />
+          <p>Health: {bot.health}</p>
+          <p>Damage: {bot.damage}</p>
+          <p>Armor: {bot.armor}</p>
+          <p>Class: {bot.bot_class}</p>
+          <button onClick={() => enlistBot(bot)}>Enlist</button>
+        </div>
+      ))}
+    </div>
+  );
+
 }
   
   export default BotCollection;
